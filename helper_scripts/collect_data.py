b697f6e738a3b58fb28414af7e8d9e0a9a37c003
pinakm9 <pinakm9@gmail.com>
2021-07-07 08:16:00 +0530

    modded collect_data

--- helper_scripts/collect_data.py
+++ helper_scripts/collect_data.py
@@ -2,11 +2,13 @@ import os
 import requests
 import tarfile
 
+# set up folders and dataset ids
 HCP_DIR = '../data'
 fnames = ["hcp_rest.tgz", "hcp_task.tgz"]
 ids = ["bqp7m", "s4h8j"]
-if not os.path.exists(HCP_DIR + '/' + fnames[1]):
-    url = "https://osf.io/{}/download/".format(ids[1])
-    response = requests.get(url, stream=True)
-    file = tarfile.open(fileobj=response.raw, mode='r|gz')
-    file.extractall(path=HCP_DIR)
+
+# download and extract data
+url = "https://osf.io/{}/download/".format(ids[1])
+response = requests.get(url, stream=True)
+file = tarfile.open(fileobj=response.raw, mode='r|gz')
+file.extractall(path=HCP_DIR)
