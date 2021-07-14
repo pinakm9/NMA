37d8e006e8fd60fe74d101336331972069e8f420
FloraNel <87109564+FloraNel@users.noreply.github.com>
2021-07-14 07:56:26 +0200

    Created ICA class

--- experiments/rest/test.py
+++ experiments/rest/test.py
@@ -6,6 +6,8 @@ from os.path import dirname, realpath
 script_dir = Path(dirname(realpath(__file__)))
 module_dir = str(script_dir.parent.parent)
 sys.path.insert(0, module_dir + '/modules')
+print(module_dir)
+print(script_dir)
 
 import rest
 from sklearn.decomposition import FastICA, PCA
