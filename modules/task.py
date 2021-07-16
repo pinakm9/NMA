f8ff835f8f19a6084c3325c1a16c6f5435a96ed6
FloraNel <87109564+FloraNel@users.noreply.github.com>
2021-07-16 11:14:57 +0200

    Updated individual and group class

--- modules/task.py
+++ modules/task.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core import einsumfunc
 
 class Individual:
     """
@@ -12,14 +13,24 @@ class Individual:
     Methods:
         load_single_timeseries: load timeseries data for a single subject and single run
         load_evs: load EVs (explanatory variables) data for one task experiment
+        load_evs_con: load EVs (explanatory variables) data for one condition of task experiment and creates timeseries
         get_regions: fetches the brain region data that's common for all the subjects
     """
 
     def __init__(self, db_path, subject_id):
         self.db_path = db_path
         self.subject_id = subject_id
-        self.experiment = 'WM'
-
+        self.exp = 'WM'
+        self.experiments = { 
+                'MOTOR'      : {'runs': [5,6],   'cond':['lf','rf','lh','rh','t','cue']},
+                'WM'         : {'runs': [7,8],   'cond':['0bk_body','0bk_faces','0bk_places','0bk_tools','2bk_body','2bk_faces','2bk_places','2bk_tools']},
+                'EMOTION'    : {'runs': [9,10],  'cond':['fear','neut']},
+                'GAMBLING'   : {'runs': [11,12], 'cond':['loss','win']},
+                'LANGUAGE'   : {'runs': [13,14], 'cond':['math','story']},
+                'RELATIONAL' : {'runs': [15,16], 'cond':['match','relation']},
+                'SOCIAL'     : {'runs': [17,18], 'cond':['mental','rnd']}
+            }
+            
     def load_single_timeseries(self, run, remove_mean=True):
         """
         Description:
@@ -41,28 +52,59 @@ class Individual:
 
 
 
-    def load_evs(self, run):
+    def load_evs(self):
         """
         Description:
             load EVs (explanatory variables) data for one task experiment
 
         Args:
             subject (int): 0-based subject ID to load
-            run (int):          rund 7 or 8 for wm task
 
         Returns
             evs (list of lists): A list of frames associated with each condition
 
         """
 
-        EXPERIMENTS = {
-            'WM'         : {'runs': [7,8],   'cond':['0bk_body','0bk_faces','0bk_places','0bk_tools','2bk_body','2bk_faces','2bk_places','2bk_tools']},
-            }
         TR = 0.72  # Time resolution, in seconds
         frames_list = []
-        task_key = 'tfMRI_'+self.experiment+['RL','LR'][run]
-        for cond in EXPERIMENTS[self.experiment]['cond']:    
-            file_path = self.db_path + '/subjects/{}/EVs/{}/{}.txt'.format(self.subject_id, task_key, cond)
+        for run in ['_RL','_LR']:
+            task_key = 'tfMRI_'+self.exp+run
+            for cond in self.experiments[self.exp]['cond']:    
+                file_path = self.db_path + '/subjects/{}/EVs/{}/{}.txt'.format(self.subject_id, task_key, cond)
+                ev_array = np.loadtxt(file_path, ndmin=2, unpack=True)
+                ev       = dict(zip(["onset", "duration", "amplitude"], ev_array))
+                # Determine when trial starts, rounded down
+                start = np.floor(ev["onset"] / TR).astype(int)
+                # Use trial duration to determine how many frames to include for trial
+                duration = np.ceil(ev["duration"] / TR).astype(int)
+                # Take the range of frames that correspond to this specific trial
+                frames = [s + np.arange(0, d) for s, d in zip(start, duration)]
+                frames_list.append(frames)
+        return frames_list
+
+
+
+
+    def load_evs_con(self, condition):
+        """
+        Description:
+            load EVs (explanatory variables) data for one condition of task experiment and creates timeseries
+
+        Args:
+            subject (int): 0-based subject ID to load
+            condition: '0bk_body','0bk_faces','0bk_places','0bk_tools','2bk_body','2bk_faces','2bk_places','2bk_tools'
+
+        Returns
+            A timeseries for all ROIs (360,78)
+
+        """          
+        TR = 0.72  # Time resolution, in seconds
+        frames_list = []
+        list_runs = [7,8]
+        ts=[]
+        for i, run in enumerate(['_RL','_LR']):
+            task_key = 'tfMRI_'+self.exp+run
+            file_path = self.db_path + '/subjects/{}/EVs/{}/{}.txt'.format(self.subject_id, task_key, condition)
             ev_array = np.loadtxt(file_path, ndmin=2, unpack=True)
             ev       = dict(zip(["onset", "duration", "amplitude"], ev_array))
             # Determine when trial starts, rounded down
@@ -71,8 +113,14 @@ class Individual:
             duration = np.ceil(ev["duration"] / TR).astype(int)
             # Take the range of frames that correspond to this specific trial
             frames = [s + np.arange(0, d) for s, d in zip(start, duration)]
-            frames_list.append(frames)
-        return frames_list
+            #frames_list.append(frames)
+            self.experiments[self.exp]['runs']
+            file_path = self.db_path + '/subjects/{}/timeseries/bold{}_Atlas_MSMAll_Glasser360Cortical.npy'.format(self.subject_id, list_runs[i])
+            newts= np.load(file_path)
+            ts.append(newts[:, frames[0]])
+        return np.hstack(ts)
+
+
 
     def get_regions(self):
         """
@@ -84,4 +132,36 @@ class Individual:
         file_path = self.db_path + '/regions.npy'
         return np.load(file_path)
 
-       
+class Group:
+    """
+    Description:
+        Class for extracting data for a condition for all subjects
+    
+    Attributes:
+        db_path: path to HCP task data
+
+    Methods:
+        extractall:
+        
+    """
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.n_subjects = 339
+
+    def extract_group_con(self, condition):
+       """
+        Description:
+            Extract all time series for all subjects for a specific condition
+
+        Args:
+            condition: '0bk_body','0bk_faces','0bk_places','0bk_tools','2bk_body','2bk_faces','2bk_places','2bk_tools'
+
+        Returns
+            A timeseries for all ROIs (360,78)
+
+        """ 
+        ts=[]
+        for n in range(339):
+            load_evs_con(condition)
+
+
