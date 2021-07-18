f7a65447b72bd346420399743c41d875bf5c08e0
pinakm9 <pinakm9@gmail.com>
2021-07-18 19:52:08 +0530

    pca comps vs svm acc

--- experiments/task/svm_roi.py
+++ experiments/task/svm_roi.py
@@ -6,21 +6,50 @@ from os.path import dirname, realpath, abspath
 script_dir = Path(dirname(realpath(__file__)))
 module_dir = str(script_dir.parent.parent)
 sys.path.insert(0, module_dir + '/modules')
-#print(module_dir)
-#print(script_dir) 
+
 
 # import remaining modules
 import task
 import methods 
+import matplotlib.pyplot as plt
 
-
-# collect data
+# set up conditions and collect list
 db_path = '../../data/hcp_task'
 group = task.Group(db_path)
-conditions = ['0bk_body', '0bk_faces', '0bk_places', '0bk_tools']
-X, y = group.extract_cons(conditions)
-print(y, X.shape)
-# fit SVM
+conditions_0 = ['0bk_body', '0bk_faces', '0bk_places', '0bk_tools']
+conditions_2 = ['2bk_body', '2bk_faces', '2bk_places', '2bk_tools']
+X_0, Y_0 = group.extract_cons(conditions_0) 
+X_2, Y_2 = group.extract_cons(conditions_2)
+
+# set up methods
+pca = methods.PCA()
+svm_0 = methods.SVM(kernel='rbf', gamma=0.7, C=1.0)
+svm_2 = methods.SVM(kernel='rbf', gamma=0.7, C=1.0)
+
+# fit SVM on entire data
+"""
 svm = methods.SVM(kernel='rbf', gamma=0.7, C=1.0)
 svm.fit(X, y)
 print("Accuracy of SVM: {}".format(svm.acc))
+""" 
+
+# fit SVM on principal components
+comp_list = list(range(2, 50))
+for n_components in comp_list:
+    pca.compute(X_0, n_components)
+    svm_0.fit(pca.new_data, Y_0)
+    pca.compute(X_2, n_components)
+    svm_2.fit(pca.new_data, Y_2)
+
+
+# plot accuarcies
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111)
+ax.plot(comp_list, svm_0.acc, c='b', label='0bk')
+ax.scatter(comp_list, svm_0.acc, c='b')
+ax.plot(comp_list, svm_2.acc, c='r', label='2bk')
+ax.scatter(comp_list, svm_2.acc, c='r')
+ax.set_xlabel("number of principal components")
+ax.set_ylabel("accuracy (%)")
+plt.legend()
+plt.savefig('../plots/task_roi_pca_comps_vs_svm_acc.png')
--- modules/methods.py
+++ modules/methods.py
@@ -1,7 +1,45 @@
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+import sklearn.decomposition 
 import utility as ut 
+import matplotlib.pyplot as plt
+
+class PCA:
+    """
+    Personalized class for PCA
+
+    Attributes:
+        n_components: number of components used in during last compute call
+        new_data: reduced data generated during last compute call
+
+    Methods:
+        compute: computes required principal components
+
+    """
+    def __init__(self):
+        pass
+
+    @ut.timer
+    def compute(self, data, n_components):
+        """
+        Description:
+            computes required principal components
+
+        Args:
+            data: data matrix of shape (samples, features)
+            n_components: number of principal components required
+
+        Returns:
+            a PCA object containing the components, variance explained data
+        """
+        self.n_components = n_components
+        pca = sklearn.decomposition.PCA(n_components=n_components)
+        self.new_data = pca.fit_transform(data)
+        return pca
+
+
+
 
 class SVM:
     """
@@ -36,5 +74,27 @@ class SVM:
         """
         clf = make_pipeline(StandardScaler(), SVC(kernel=self.kernel, **self.params))
         clf.fit(data, labels)
-        self.acc = clf.score(data, labels)
-        return clf
+        if not hasattr(self, 'acc'):
+            self.acc = clf.score(data, labels) * 100.0
+        elif isinstance(self.acc, list):
+            self.acc.append(clf.score(data, labels) * 100.0)
+        else:
+            self.acc = [self.acc]
+            self.acc.append(clf.score(data, labels) * 100.0)
+        return clf 
+
+    def plot_acc(self, x_data, file_path):
+        """
+        Description:
+            plots accuracies
+        
+        Args:
+            x_data: x values corresponding to accuracies
+            file_path: path for saving the plot
+        """
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111)
+        ax.plot(x_data, self.acc)
+        ax.scatter(x_data, self.acc)
+        ax.set_ylabel("accuracy")
+        plt.savefig(file_path)
